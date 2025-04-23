54bd7a4d93754bd3aebecbc485772ffd38d838c3
Sid-Tek <98853457+Sid-Tek@users.noreply.github.com>
2025-04-23 09:31:18 -0400

    Update pokemon.r

--- Code/pokemon.r
+++ Code/pokemon.r
@@ -1,5 +1,20 @@
 # List the needed visualizations here, as a comment. Each member will tackle a visualization independently, then post the code here.
 
-# We should look at attack / special attack vs speed (to search for glass cannons)
 
-# We should also look at defense / special defense vs hp (to search for bulky defenders)
+
+
+# Look at stat distributions by type in a table
+
+
+
+
+
+
+# Make a scatterplot, with each point being a Pokemon, of attack/speed 
+
+
+
+
+
+
+# Make a scatterplot, with each point being a Pokemon, of defense/hp
