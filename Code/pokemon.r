cf94763b87e1fadc7d51bc25970f870cea70c096
Sid-Tek <98853457+Sid-Tek@users.noreply.github.com>
2025-04-23 09:14:03 -0400

    Update pokemon.r

--- Code/pokemon.r
+++ Code/pokemon.r
@@ -1,1 +1,5 @@
 # List the needed visualizations here, as a comment. Each member will tackle a visualization independently, then post the code here.
+
+# We should look at attack / special attack vs speed (to search for glass cannons)
+
+# We should also look at defense / special defense vs hp (to search for bulky defenders)
